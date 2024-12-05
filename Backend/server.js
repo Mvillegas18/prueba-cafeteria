d1a64f84171394ee0d93c1c291a18a5edd0e6678
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-04 20:10:51 -0500

    Remove CORS middleware from server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,8 @@
-import cors from 'cors';
 import express from 'express';
 import { route } from './app.js';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use('/api', route);
 

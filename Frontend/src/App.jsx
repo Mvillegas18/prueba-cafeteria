87052ef443d8890877fc6eb62926a8a7b1f2fd98
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-04 21:28:25 -0500

    Add Tailwind CSS setup and update App component styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-	return <div>Aplicacion cafeteria</div>;
+	return (
+		<div className='text-3xl font-bold flex justify-center my-5'>
+			Aplicacion cafeteria
+		</div>
+	);
 }

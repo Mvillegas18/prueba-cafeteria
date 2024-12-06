125736248bd2545f51c6aa36235b5c52c77bf5e7
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-06 14:12:32 -0500

    Update server port configuration to 5173

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/api/products', route);
 app.use('/api/sales', routeSales);
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 5173;
 app.listen(PORT, () => {
 	console.log(`Servidor en: http://localhost:${PORT}`);
 });

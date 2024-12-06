12b9390e9b58a7795c40b607dfaf9c3a1977b0b3
Miguel Angel Villegas <chumilo227@gmail.com>
2024-12-06 14:28:33 -0500

    Add CORS middleware to server for handling cross-origin requests

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,19 @@
+import cors from 'cors';
 import express from 'express';
+
 import { route } from './appProducts.js';
 import { routeSales } from './appSales.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		methods: 'GET,POST,PUT,DELETE',
+		allowedHeaders: 'Content-Type,Authorization',
+	})
+);
 
 app.use('/api/products', route);
 app.use('/api/sales', routeSales);
